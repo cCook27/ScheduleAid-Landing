da080585a3790da1dc24d91a1cd01c48f08b0056
Connor Cook <97connorcook@gmail.com>
2024-02-02 11:16:53 -0700

    Email can't be clicked unless there is text in the input

--- src/components/action-button.js
+++ src/components/action-button.js
@@ -5,6 +5,9 @@ import '../css/action-button.css';
 
 const ActionButton = ({ handleAlerts }) => {
   const form = useRef();
+  const [userName, setUserName] = useState('');
+  const [userEmail, setUserEmail] = useState('');
+  const isFormFilled = userName !== '' && userEmail !== '';
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -12,16 +15,27 @@ const ActionButton = ({ handleAlerts }) => {
     emailjs.sendForm('service_7h6l1j2', 'template_udwfu8r', form.current, 'wLYZCwyNGuTe-MHOZ')
       .then((result) => {
         handleAlerts(result.text);
+        setUserName('');
+        setUserEmail('');
+
       }, (error) => {
           console.log(error.text);
       });
   };
 
+  const handleNameChange = (event) => {
+        setUserName(event.target.value);
+    };
+
+  const handleEmailChange = (event) => {
+      setUserEmail(event.target.value);
+  };
+
   return (
     <form ref={form} onSubmit={sendEmail}>
-      <input className='form-control my-2' placeholder='Jane Doe' type="text" name="user_name" />
-      <input className='form-control my-2' type="email" placeholder='example@email.com' name="user_email" />
-      <input className='reserve' type="submit" value="Stay in the Loop!" />
+      <input className='form-control my-2' placeholder='Jane Doe' type="text" name="user_name" value={userName} onChange={handleNameChange} />
+      <input className='form-control my-2' type="email" placeholder='example@email.com' name="user_email" value={userEmail} onChange={handleEmailChange} />
+      <button className='reserve' type="submit" disabled={!isFormFilled}>Stay in the Loop!</button>
     </form>
   );
 };
